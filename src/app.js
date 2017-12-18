d71134824b65cf694d909698e886c4548dcf4edc
Sara Frandsen <sara.frandsen@gmail.com>
2017-12-18 14:40:30 -0800

    created base html

--- src/app.js
+++ src/app.js
@@ -1,13 +1,28 @@
-import 'css/_settings.css';
-import 'foundation-sites/dist/css/foundation.css';
-import './css/styles.css';
-
-// Import jQuery & Underscore
 import $ from 'jquery';
 import _ from 'underscore';
+// CSS
+import './css/foundation.css';
+import 'css/_settings.css';
+import './css/style.css';
+
+const MOVIE_FIELDS = ['id', 'title', 'overview', 'release_date', 'image_url'];
+
+// TEMPLATES
+let moviesTemplate;
+// let overviewTemplate;
+// let createNewMovieTemplate;
+
+const renderMovies = function renderMovies(movieList) {
+  const moviesTableElement = $('#movie-list-table');
+  moviesTableElement.html('');
+}
 
 // ready to go
 $(document).ready(function() {
+  // TEMPLATES
+  moviesTemplate = _.template($('#movies-template').html());
+  // overviewTemplate = _.template($('#movie-template').html());
+  // createNewMovieTemplate = _.template($('#create-new-movie-template').html());
 
   $('#main-content').append('<p>Hello World!</p>');
 
