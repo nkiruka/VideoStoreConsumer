2ae795989065d3e4eb11b13f5bb6475b4b5f9a35
Amy Cash <amymcash@gmail.com>
2017-12-18 14:24:58 -0800

    More set-up

--- src/collections/movie_list.js
+++ src/collections/movie_list.js
@@ -3,6 +3,11 @@ import Movie from 'models/movie';
 
 const MovieList = Backbone.Collection.extend({
   model: Movie,
+  url: 'https://api.themoviedb.org/3/'
 });
 
 export default MovieList;
+
+
+
+  // "https://api.themoviedb.org/3/" "search/movie?api_key=""_____&query="query"
--- src/models/movie.js
+++ src/models/movie.js
@@ -3,10 +3,10 @@ import Backbone from 'backbone';
 
 const Movie = Backbone.Model.extend({
 
+  urlRoot: 'https://api.themoviedb.org/3/',
 
 
-
-
+//validations here?
 
 
 });
