ebfe5debe529ef344b9a13fef1e5828467030795
Amy Cash <amymcash@gmail.com>
2017-12-19 12:15:12 -0800

    Set up movie list view

--- src/views/movie_list_view.js
+++ src/views/movie_list_view.js
@@ -9,17 +9,26 @@ import Movie from '../models/movie';
 const MovieListView = Backbone.View.extend({
 
   initialize(params) {
-
+    this.template = params.template;
+    this.listenTo(this.model, 'update', this.render);
   },
 
   render(){
     console.log("In Movie List render function");
+    this.$('#library-movies').empty();
+    this.model.each((movie) => {
+      const movieView = new MovieView ({
+        model: movie,
+        template: this.template,
+        tagName: 'li',
+        className: 'movie',
+      });
 
-    return this
+      this.$('#library-movies').append(movieView.render().$el);
+
+    });
+    return this;
   },
-
-
-
 }); // end movie list view
 
 export default MovieListView;
